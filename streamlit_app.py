06a56f8bfa527ddfd9b0f0c8161d4a488a7708c9
kathryn-snowflake <150055768+kathryn-snowflake@users.noreply.github.com>
2023-11-13 14:28:55 +0000

    Update streamlit_app.py
    typo error

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text.input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user enteres', fruit choice)
+streamlit.write('The user enteres', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
